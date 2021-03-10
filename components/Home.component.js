c07b16e3858c33c3f14635495fa585b432a8cdbb
Joohoon Kim <jayjhkim7@gmail.com>
2021-03-09 23:38:27 -0500

    added home component

--- components/Home.component.js
+++ components/Home.component.js
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+const RenderItem = ({ item }) => {
+  if (item) {
+    console.log('ITEMNAME: ', item.name);
+    //TODO: item name not displayiing over the image
+    return (
+      <Card
+        featuredtitle={item.name}
+        image={require('./images/react-lake.jpg')}
+      >
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
+};
+
 const Home = (props) => {
+  const [campsites, setCampsites] = useState(CAMPSITES);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [partners, setPartners] = useState(PARTNERS);
   return (
-    <View>
-      <Text>HOME component</Text>
-    </View>
+    <ScrollView>
+      <RenderItem
+        item={campsites.filter((campsite) => campsite.featured === true)[0]}
+      />
+      <RenderItem
+        item={promotions.filter((promotion) => promotion.featured)[0]}
+      />
+      <RenderItem item={partners.filter((partner) => partner.featured)[0]} />
+    </ScrollView>
   );
 };
 
