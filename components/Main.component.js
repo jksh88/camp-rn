7062e696305e1ce32f4c3bcaaaf30fc3251bbada
Joohoon Kim <jayjhkim7@gmail.com>
2021-03-13 23:43:48 -0500

    navigation icons wip

--- components/Main.component.js
+++ components/Main.component.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Directory from './Directory.component';
 import CampsiteInfo from './CampsiteInfo.component';
-import { View, Platform } from 'react-native';
+import { View, Platform, StyleSheet } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -9,6 +9,7 @@ import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 import Home from './Home.component';
 import About from './About.component';
 import Contact from './Contact.component';
+import { Icon } from 'react-native-elements';
 
 //createStackNavigator has one required argument called route configs object where we set what components will be available(in this case Directory and CampsiteInfo components)
 // https://reactnavigation.org/docs/4.x/headers
@@ -16,7 +17,19 @@ import Contact from './Contact.component';
 // navigate, goBack, addListener, isFocused, state, setParams
 const DirectoryNavigator = createStackNavigator(
   {
-    Directory: { screen: Directory },
+    Directory: {
+      screen: Directory,
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: (
+          <Icon
+            name="list"
+            type="font-awesome"
+            iconStyle={styles.stackIcon}
+            onPress={() => navigation.toggleDrawer()}
+          />
+        ),
+      }),
+    },
     CampsiteInfo: { screen: CampsiteInfo },
     //Each screen component in your app is provided with the navigation prop automatically.
   },
@@ -39,7 +52,7 @@ const HomeNavigator = createStackNavigator(
     Home: { screen: Home },
   },
   {
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({ navigation }) => ({
       headerStyle: {
         backgroundColor: '#5637DD',
       },
@@ -47,7 +60,15 @@ const HomeNavigator = createStackNavigator(
       headerTitleStyle: {
         color: '#fff',
       },
-    },
+      headerLeft: (
+        <Icon
+          name="home"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
   }
 );
 
@@ -56,7 +77,7 @@ const AboutNavigator = createStackNavigator(
     About: { screen: About },
   },
   {
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({ navigation }) => ({
       headerStyle: {
         backgroundColor: '#5637DD',
       },
@@ -64,7 +85,15 @@ const AboutNavigator = createStackNavigator(
       headerTitleStyle: {
         color: '#fff',
       },
-    },
+      headerLeft: (
+        <Icon
+          name="info-circle"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
   }
 );
 
@@ -73,7 +102,7 @@ const ContactNavigator = createStackNavigator(
     Contact: { screen: Contact },
   },
   {
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({ navigation }) => ({
       headerStyle: {
         backgroundColor: '#5637DD',
       },
@@ -81,16 +110,64 @@ const ContactNavigator = createStackNavigator(
       headerTitleStyle: {
         color: '#fff',
       },
-    },
+      headerLeft: (
+        <Icon
+          name="address-card"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
   }
 );
 
 const MainNavigator = createDrawerNavigator(
   {
-    Home: { screen: HomeNavigator },
-    Directory: { screen: DirectoryNavigator },
-    About: { screen: AboutNavigator },
-    Contact: { screen: ContactNavigator },
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="home" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
+    },
+    Directory: {
+      screen: DirectoryNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="list" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
+    },
+    About: {
+      screen: AboutNavigator,
+      navigationOptions: {
+        drawerLabel: 'About Us',
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name="info-circle"
+            type="font-awesome"
+            size={24}
+            color={tintColor}
+          />
+        ),
+      },
+    },
+    Contact: {
+      screen: ContactNavigator,
+      navigationOptions: {
+        drawerLabel: 'Contact Us',
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name="access-card"
+            type="font-awesome"
+            size={24}
+            color={tintColor}
+          />
+        ),
+      },
+    },
   },
   {
     drawerBackgroundColor: '#CEC8FF',
@@ -103,6 +180,8 @@ const MainNavigator = createDrawerNavigator(
 //App's top-level nagvigator is typically wrapped in createAppContainer like this.
 //Here, the execution result of the function is assigned to a constant AppNavigator.
 const AppNavigator = createAppContainer(MainNavigator);
+//Containers are responsible for managing your app state and linking your top-level navigator to the app environment. On Android, the app container uses the Linking API
+//to handle the back button. The container can also be configured to persist your navigation state. On web, you'd use different containers than React Native.
 
 const Main = (props) => {
   return (
@@ -116,5 +195,13 @@ const Main = (props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  stackIcon: {
+    marginLeft: 10,
+    color: '#fff',
+    fontSize: 24,
+  },
+});
 
 export default Main;
