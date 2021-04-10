4955b7a3861b3276fa1324e0f785efca9c0bedf1
Joohoon Kim <jayjhkim7@gmail.com>
2021-04-10 18:20:34 -0400

    reservation wip issues with non-closing datepicker to be resolved

--- components/Reservation.component.js
+++ components/Reservation.component.js
@@ -7,6 +7,7 @@ import {
   Switch,
   Button,
 } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Reservation = (props) => {
@@ -15,6 +16,12 @@ const Reservation = (props) => {
   const [date, setDate] = useState(new Date());
   const [showCalendar, setShowCalendar] = useState(false);
 
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    // setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
   const handleReservation = () => {
     console.log(JSON.stringify({ campers, hikeIn, date, showCalendar }));
     setCampers(1);
@@ -22,11 +29,59 @@ const Reservation = (props) => {
     setDate(new Date());
     setShowCalendar(false);
   };
-
+  //https://blog.logrocket.com/how-to-use-the-react-native-picker-select/
+  // https://www.npmjs.com/package/react-native-picker-select/
+  // https://reactnative.dev/docs/switch
+  console.log('CAMPERS: ', campers);
   return (
     <ScrollView>
-      <View>
+      <View style={styles.formRow}>
         <Text>Number of Campers</Text>
+        <RNPickerSelect
+          onValueChange={(value) => setCampers(value)}
+          value={campers}
+          items={[
+            { label: '1', value: '1' },
+            { label: '2', value: '2' },
+            { label: '3', value: '3' },
+            { label: '4', value: '4' },
+            { label: '5', value: '5' },
+            { label: '6', value: '6' },
+          ]}
+        />
+      </View>
+      <View style={styles.formRow}>
+        <Text>Hike-In?</Text>
+        <Switch
+          value={hikeIn}
+          onValueChange={(value) => setHikeIn(value)}
+          trackColor={{ true: 'green', false: null }}
+        />
+      </View>
+      <View style={styles.formRow}>
+        <Text style={styles.formLabel}>Date</Text>
+        <Button
+          onPress={() => setShowCalendar((setShowCalendar) => !setShowCalendar)}
+          title="here"
+          color="#5637DD"
+          accessibilityLabel="Tap to select the reservation date"
+        />
+      </View>
+      {showCalendar && (
+        <DateTimePicker
+          value={date}
+          onChange={onChange}
+          display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+          mode={'date'}
+        />
+      )}
+      <View style={styles.formRow}>
+        <Button
+          onPress={() => handleReservation()}
+          title="Search"
+          color="#5637DD"
+          accessibilityLabel="Tap to search for available campsites to reserve"
+        />
       </View>
     </ScrollView>
   );
@@ -34,6 +89,22 @@ const Reservation = (props) => {
 
 Reservation.navigationOptions = () => ({
   title: 'Reserve campsite',
+});
+
+const styles = StyleSheet.create({
+  formRow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    margin: 20,
+  },
+  formLabel: {
+    fontSize: 18,
+  },
+  formItem: {
+    flex: 1,
+  },
 });
 
 export default Reservation;
